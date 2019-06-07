1f57b1891c609bb3c85611ea2609f74af40f537f
parkdongsu <parkdongsu>
2019-06-07 10:58:43 +0900

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+from cx_Freeze import setup, Executable
+
+setup(
+        name="newMedicalTermsDbBuildingTool",
+        version="1.0",
+        description="",
+        author="DongsuPark",
+        executables = [Executable("Crolling.py")]
+)
+
+
